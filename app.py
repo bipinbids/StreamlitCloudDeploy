5bc9f942846a66c64141f7d65f8831f646d61116
Bipin Biddappa <bipinbids@gmail.com>
2024-04-11 13:43:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ button_clicked = st.sidebar.button('Show Pie Chart')
 if button_clicked:
     st.write('Pie Chart:')
     data = np.random.rand(5)
-    fig, ax = plt.subplots(figsize=(6, 1))
+    fig, ax = plt.subplots()
     ax.pie(data, labels=[f'x{i}' for i in range(1, 6)])
     st.pyplot(fig)
+
+    # Set the width of the chart to fit the screen
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.pyplot(fig, clear_figure=True)
