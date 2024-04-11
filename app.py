ad0f67cfb2db40068ad8bb0d59b00784500d73e4
Bipin Biddappa <bipinbids@gmail.com>
2024-04-11 13:51:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,30 @@ import numpy as np
 st.sidebar.title('Sidebar Title')
 
 # Add a button to the sidebar
-button_clicked = st.sidebar.button('Show Pie Chart')
+Pie_button_clicked = st.sidebar.button('Pie Chart')
+Bar_button_clicked = st.sidebar.button('Bar Chart')
 
 # Display a pie chart when the button is clicked
-if button_clicked:
-    st.write('Pie Chart:')
+if Pie_button_clicked:
+    st.write('There could be text here which talks about the Pie Chart below')
     data = np.random.rand(5)
     fig, ax = plt.subplots()
     ax.pie(data, labels=[f'x{i}' for i in range(1, 6)])
+    #st.pyplot(fig)
+
+    # Set the width of the chart to fit the screen
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.pyplot(fig, clear_figure=True)
+
+
+if Bar_button_clicked:
+    st.write('There could be text here which talks about the bar chart below')
+
+    
+    data = np.random.rand(5)
+    labels = [f'x{i}' for i in range(1, 6)]
+    fig, ax = plt.subplots()
+    ax.bar(labels, data)
     #st.pyplot(fig)
 
     # Set the width of the chart to fit the screen
