ce88315dd19e4ed438901c1eb9b2906d055f1635
Bipin Biddappa <bipinbids@gmail.com>
2024-04-11 13:32:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
 import streamlit as st
-st.title("This is me Checking if this works")
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Add a title to the sidebar
+st.sidebar.title('Sidebar Title')
+
+# Add a button to the sidebar
+button_clicked = st.sidebar.button('Show Pie Chart')
+
+# Display a pie chart when the button is clicked
+if button_clicked:
+    st.write('Pie Chart:')
+    data = np.random.rand(5)
+    plt.pie(data, labels=[f'x{i}' for i in range(1, 6)])
+    st.pyplot(plt)
