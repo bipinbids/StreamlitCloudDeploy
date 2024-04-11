49e1ca50357e75af6eab0735b10a8908575dfa5a
Bipin Biddappa <bipinbids@gmail.com>
2024-04-11 13:42:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,6 @@ button_clicked = st.sidebar.button('Show Pie Chart')
 if button_clicked:
     st.write('Pie Chart:')
     data = np.random.rand(5)
-    fig, ax = plt.subplots(figsize=(6, 4))
+    fig, ax = plt.subplots(figsize=(6, 1))
     ax.pie(data, labels=[f'x{i}' for i in range(1, 6)])
     st.pyplot(fig)
